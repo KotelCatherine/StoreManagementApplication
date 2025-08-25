14d2755e190890a117d1dfbb4ef0b4ab86788bb9
cotelCatherine <bezmozggoff@gmail.com>
2025-08-26 01:04:17 +0700

    Initial commit

---
+++ src/test/java/com/example/store/controller/StoreManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.store.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
